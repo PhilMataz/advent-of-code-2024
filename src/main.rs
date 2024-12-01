0e9efde75162476a2c83ee85293b0b225890a6d0
Phillip Matambanadzo <hello@philmataz.dev>
2024-12-01 16:36:45 +0000

    second star solution

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let contents = fs::read_to_string("input.txt")
@@ -22,5 +23,21 @@ fn main() {
     
     let result: i32 = (0..first_list.len()).map(|x| (second_list[x] - first_list[x]).abs()).reduce(|acc, e| acc + e).unwrap();
     println!("{:#?}", result);
+
+    let mut second_list_count: HashMap<i32, i32> = HashMap::new();
+
+    second_list.into_iter().for_each(|el| {
+        let count = second_list_count.entry(el).or_insert(0);
+        *count += 1;
+    });
+
+
+
+     let result: i32 = first_list.into_iter().fold(0, |acc, el| {
+        acc + second_list_count.get(&el).unwrap_or(&0) * el
+    });
+   
+    println!("{:#?}", result);
+    
 }
 
