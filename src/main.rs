fa015f803347f76ead3bf08b116a8f236f8453cb
Phillip Matambanadzo <hello@philmataz.dev>
2024-12-01 15:47:56 +0000

    first start solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input.txt")
+        .expect("Error reading file!");
+
+    
+    let mut first_list: Vec<i32> = vec![];
+    let mut second_list: Vec<i32> = vec![];
+
+    for line in contents.lines() {
+        let mut remainder = line.trim().split("   ");
+
+        // remainder.for_each(|el| println!("The element: {}", el));
+        first_list.push(remainder.next().expect("Error while iterating line!").parse().expect("Error while parsing"));
+        second_list.push(remainder.next().expect("Error while iterating line!").parse().expect("Error while parsing"));
+    }
+    first_list.sort();
+    second_list.sort();
+
+
+    
+    let result: i32 = (0..first_list.len()).map(|x| (second_list[x] - first_list[x]).abs()).reduce(|acc, e| acc + e).unwrap();
+    println!("{:#?}", result);
 }
+
