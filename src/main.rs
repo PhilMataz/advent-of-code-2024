01f45c1b372d06dc0998b3ca6dd0ee8142750b0d
Phillip Matambanadzo <hello@philmataz.dev>
2024-12-02 13:52:38 +0000

    second star

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::error;
 fn main() -> Result<(), Box<dyn error::Error + 'static>> {
     let input = fs::read_to_string("input.txt")?;
     
+    let mut data_with_deltas: Vec<Vec<i32>> = vec![];
+    
     let data: Vec<Vec<u32>> = input
         .lines()
         .map(|line| line
@@ -14,25 +16,44 @@ fn main() -> Result<(), Box<dyn error::Error + 'static>> {
                 }).collect()
         )
         .filter(|line: &Vec<u32>| {
-            let mut is_valid = true;
-            let mut is_increasing = true;
-            let mut is_decreasing = true;
-            for i in 1..line.len() {
-                let diff = (line[i-1] as i32 - line[i] as i32).abs();
-                if diff < 1 || diff > 3 {
-                    is_valid = false; 
+            let mut pass = false;
+
+            for i in 0..line.len() {
+                let mut copy = line.clone();
+                copy.remove(i);
+                if is_safe(&copy) {
+                    pass = true;
                     break;
                 }
-                if line[i-1] < line[i] {
-                    is_decreasing=false;
-                }
-                if line[i-1] > line [i] {
-                    is_increasing = false;
-                }
             }
-            is_valid && (is_increasing || is_decreasing)
+            println!("line: {:#?}", line);
+            println!("pass: {}", pass);
+
+            pass
         }).collect();
     println!("Valid datasets: {:#?}", data.len());
+
+    fn is_safe(line: &Vec<u32>) -> bool {
+        let mut is_valid = true;
+        let initial_trend = line[0] < line[1]; 
+        let mut has_trend_changed = false;
+        
+        let mut i = 0;
+        while i  < (line.len() - 1) && is_valid && !has_trend_changed {
+            let diff = (line[i+1] as i32 - line[i] as i32).abs();
+            if diff < 1 || diff > 3 {
+                is_valid = false; 
+            } 
+            let current_tend = line[i] < line[i+1];
+            if current_tend != initial_trend {
+                has_trend_changed = true;
+            }
+
+            i += 1;
+        }
+        is_valid && !has_trend_changed
+    }
+
     Ok(())
 }
 
